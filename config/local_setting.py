228bb47d2411c1adff9916469cf797faa03154ab
jxch <15553598935@163.com>
2022-05-25 19:23:55 +0800

    local config

--- config/local_setting.py
+++ config/local_setting.py
@@ -5,3 +5,4 @@ class LocalSetting(base_setting.BaseSetting):
     DEBUG = False
     SERVER_PORT = 8080
     APP_NAME = "Capital-Breath-A[local]"
+    DB_CAPITAL = 'mysql+pymysql://root:jiang155.@aws.jiangxicheng.xyz:3306/capital'
