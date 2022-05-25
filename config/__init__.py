a2ede48bf423ab998ebf50d71a82a57851ef87f4
jxch <15553598935@163.com>
2022-05-25 19:02:24 +0800

    config

---
+++ config/__init__.py
@@ -0,0 +1,18 @@
+import tushare as ts
+
+from . import dev_setting
+from . import test_setting
+from . import local_setting
+from . import product_setting
+from . import product_china_setting
+
+config_dict = {
+    'default': local_setting.LocalSetting,
+    'dev': dev_setting.DevSetting,
+    'test': test_setting.TestSetting,
+    'local': local_setting.LocalSetting,
+    'product': product_setting.ProductSetting,
+    'product_china': product_china_setting.ProductChinaSetting
+}
+
+pro = ts.pro_api('34af7b2e7f3bbf2c48b0a12e0084c9099df78acd66f37ad5c92e0326')
---
+++ config/base_setting.py
@@ -0,0 +1,9 @@
+class BaseSetting:
+    DEBUG = True
+    THREADED = True
+    SERVER_PORT = 8080
+    APP_NAME = "Capital-Breath-A"
+    SERVER_HOST = "localhost"
+    HOST = "0.0.0.0"
+    EUREKA_SERVER = "http://admin:admin@jkm.jiangxicheng.xyz:10001/eureka"
+    DB_CAPITAL = 'mysql+pymysql://root:jiang155.@jiangxicheng.xyz:3306/capital'
